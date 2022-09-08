57328380b3e645d76a6cbad8ed71df9f5abb3979
EddieTaing <110514250+EddieTaing@users.noreply.github.com>
2022-09-08 12:31:44 +1000

    Add files via upload
    Completed addsales.php. Works and tested

--- addsales.php
+++ addsales.php
@@ -9,8 +9,9 @@
     <body>
         <?php
             require_once("settings.php");
+
             //Validate inputs
-            if (isset($_POST['member_id']) && isset($_POST['transaction_id']) && isset($_POST['item_name']) && isset($_POST['item_quantity']) && isset($$_POST['datetime_sold']))
+            if (isset($_POST['member_id']) && isset($_POST['transaction_id']) && isset($_POST['item_name']) && isset($_POST['item_quantity']) && isset($_POST['date_sold']))
             {
                 //Variable to hold errors
                 $errorMsg = [];
@@ -22,6 +23,7 @@
                 if (strlen($member_id) > 0 && is_numeric($member_id))
                 {
                     //Check if member id in table
+                    //Creating database connection.
                     $conn = new mysqli($host, $user, $pswd, $db);
 
                     //Check if error with connection
@@ -31,17 +33,20 @@
                         exit();
                     }
 
-                    //Checking that member is in database.
-                    $checkmember = "SELECT * FROM members WHERE member_id = `$member_id`;";
+                    $member_id = (int) $member_id;
+
+                    //Query to check that member is in database.
+                    $checkmember = "SELECT * FROM members WHERE member_id = $member_id";
 
                     //Querying database.
                     $result = $conn->query($checkmember);
 
                     //Checking result from query
-                    if ($result === TRUE)
-                    {
+                    if ($result == TRUE)
+                    {      
                         //Checking if member data was retrieved from database.
                         $rows = mysqli_num_rows($result);
+
                         if ($rows != 1)
                         {
                             $errorMsg[] = "Member with id $member_id does not exist.";
@@ -49,7 +54,7 @@
                     }
                     else
                     {
-                        $errorMsg[] = $conn->error;
+                        $errorMsg[] = "Error with SQL: [$checkmember]. " . $conn->error;
                     }
 
                     //Closing connection
@@ -66,6 +71,10 @@
                     if (strlen($transaction_id) != 8)
                     {
                         $errorMsg[] = "Transaction ID ($transaction_id) is not 8 in length";
+                    }
+                    else
+                    {
+                        $transaction_id = (int) $transaction_id;
                     }
                 }
                 else
@@ -90,10 +99,39 @@
                     $errorMsg[] = "Item name must be between 1 and 20 characters long (inclusive).";
                 }
 
-                /*
-                    Finish Validation
-                    Maybe change datetime_sold to delivery_date
-                */
+                //Validating item quantity
+                $item_quantity = $_POST['item_quantity'];
+
+                if (is_numeric($item_quantity))
+                {
+                    if ((int) $item_quantity < 1)
+                    {
+                        $errorMsg[] = "Item quantity must be 1 or greater.";
+                    }
+                    else
+                    {
+                        $item_quantity = (int) $item_quantity;
+                    }
+                }
+                else
+                {
+                    $errorMsg[] = "Item quantity is not a number.";
+                }
+
+
+                //Validating date
+                $date_sold = $_POST['date_sold'];
+                //https://stackoverflow.com/questions/30243775/get-date-from-input-form-within-php
+
+                if (empty($date_sold))
+                {
+                    $errorMsg[] = "Date sold has not been set.";
+                }
+                else
+                {
+                    $new_date = strtotime($date_sold);
+                    $new_date = date("Y-m-d", $new_date);
+                }
 
                 if (sizeof($errorMsg) == 0)
                 {
@@ -115,25 +153,55 @@
                         transaction_id INT NOT NULL,
                         item_name varchar(20) NOT NULL,
                         item_quantity INT NOT NULL,
-                        datetime_sold DATETIME NOT NULL,
+                        date_sold DATE NOT NULL,
                         PRIMARY KEY (sales_id),
                         FOREIGN KEY (member_id) REFERENCES members(member_id)
                     );";
 
                     //Execute table creation query
-                    $conn->query($tablequery);
+                    if ($conn->query($tablequery) === FALSE)
+                    {
+                        echo "<p>Error with database: " . $conn->error . "</p>";
+                    }
 
+                    //Query to insert new sales into sales records table with form data.
+                    $insertsales = "INSERT INTO `sales_records` (`member_id`, `transaction_id`, `item_name`, `item_quantity`, `date_sold`) VALUES ('$member_id', '$transaction_id', '$item_name', '$item_quantity', '$date_sold');";
+
+
+                    //Checking if query entered successfully.
+                    if ($conn->query($insertsales) === TRUE)
+                    {
+                        echo "<p>Sales record has been successfully entered into system</p>";
+                    }
+                    else
+                    {
+                        echo "<p>Error with entering sales: " . $conn->error ."</p>";
+                    }
+
+                    //Closing connection
                     $conn->close();
                 }
                 else
                 {
-                    //Display errors here
+                    //Display errors here in unordered list
+                    echo "<p>The following error(s) have been encountered:</p>";
+
+                    echo "<ul>";
+                    foreach($errorMsg as $error)
+                    {
+                        if ($error != "")
+                        {
+                            echo "<li>$error</li>";
+                        }
+                    }
+
+                    echo "</ul>";
                 }
             }
-            //If errors, display errors
-            //If no errors, add input data to database
-
-            
+            else
+            {
+                echo "<p>Form data has not been set</p>";
+            }
         ?>
     </body>
 </html>
