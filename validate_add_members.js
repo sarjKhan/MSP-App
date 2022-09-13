927797c007b7190d38402f0f727359def3420793
sarjKhan <102861209+sarjKhan@users.noreply.github.com>
2022-09-13 17:25:53 +1000

    Edited HTML

--- validate_add_members.js
+++ validate_add_members.js
@@ -3,17 +3,17 @@
 var fnameError = document.getElementById('fname_error');
 var lnameError = document.getElementById('lname_error');
 var phoneError= document.getElementById('phone_error');
-var addressError = document.getElementById('address_error');
+var emailError = document.getElementById('email_error');
 var submitError = document.getElementById('submit-error')
 
 function validateFName(){
 	var fname = document.getElementById('f_name').value;
 	if (fname.length == 0 || fname.length <=2 || fname== null){
-		fnameError.innerHTML = 'Cannot be empty or less than 3 characters';	
+		fnameError.innerHTML = '<br>Cannot be empty or less than 3 characters';	
 		return false;
 	}
 	if(!fname.match(/^[a-zA-Z]+$/)) {
-		fnameError.innerHTML = 'Must be correct letter format';
+		fnameError.innerHTML = '<br>Must be correct letter format';
 		return false;
 	}
 
@@ -25,11 +25,11 @@ function validateFName(){
 function validateLName(){
 	var lname = document.getElementById('l_name').value;
 	if (lname.length == 0 || lname== null){
-		lnameError.innerHTML = 'Cannot be empty';	
+		lnameError.innerHTML = '<br>Cannot be empty';	
 		return false;
 	}
 	if(!lname.match(/^[a-zA-Z]+$/)) {
-		lnameError.innerHTML = 'Must be correct letter format';
+		lnameError.innerHTML = '<br>Must be correct letter format';
 		return false;
 	}
 	lnameError.innerHTML ='<i class="fa-solid fa-circle-check"></i>';
@@ -39,28 +39,28 @@ function validateLName(){
 function validatePhone(){
 	var phone = document.getElementById('phone').value;
 	if (phone.length == 0 || phone== null){
-		phoneError.innerHTML = 'Cannot be empty';	
+		phoneError.innerHTML = '<br>Cannot be empty';	
 		return false;
 	}
 	if (phone.length != 10){
-		phoneError.innerHTML = 'Phone number must be 10 digits';	
+		phoneError.innerHTML = '<br>Phone number must be 10 digits';	
 		return false;
 	}
 	if(!phone.match(/^[0-9]{10}$/)) {
-		phoneError.innerHTML = 'Only number format';
+		phoneError.innerHTML = '<br>Only number format';
 		return false;
 	}
 	phoneError.innerHTML ='<i class="fa-solid fa-circle-check"></i>';
 	return true;
 }
 
-function validateAddress(){
-	var address = document.getElementById('address').value;
-	if (address.length == 0 || address == null){
-		addressError.innerHTML = 'Cannot be empty';	
+function validateEmail(){
+	var email = document.getElementById('email').value;
+	if (email.length == 0 || email == null){
+		emailError.innerHTML = '<br>Cannot be empty';	
 		return false;
 	}
-	addressError.innerHTML ='<i class="fa-solid fa-circle-check"></i>';
+	emailError.innerHTML ='<i class="fa-solid fa-circle-check"></i>';
 	return true;
 }
 
