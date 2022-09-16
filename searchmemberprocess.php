2027984c082ae6cdb3c4e3688fe583aa06e94516
EddieTaing <110514250+EddieTaing@users.noreply.github.com>
2022-09-16 15:37:57 +1000

    Adding search php files
    Added completed and tested search sales and search member files.

---
+++ searchmemberprocess.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+    <meta name="Title" content="GotoGro Memeber Management Sysytem"/>
+   	<meta name="Authors" content="Sartaj Khan, Eddie Taing"/>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+	<?php
+		require_once("settings.php");
+
+		$selectquery = "SELECT * FROM `members` WHERE ";
+		$criteriacount = 0;
+
+		//Validating member id
+		if (isset($_POST['member_id']))
+		{
+			$member_id = $_POST['member_id'];
+			if (is_numeric($member_id) && $member_id)
+			{
+				//Adding member id to query
+				$selectquery .= "`member_id`=$member_id AND ";
+				$criteriacount++;
+			}
+		}
+
+		//Validating first name
+		if (isset($_POST['f_name']))
+		{
+			$f_name = $_POST['f_name'];
+			$pattern = "/^[a-zA-Z- ]+$/";
+			if (strlen($f_name) > 0 && preg_match($pattern, $f_name))
+			{
+				//Adding first name to query
+				$selectquery .= "`fname`='$f_name' AND ";
+				$criteriacount++;
+			}
+		}
+
+		//Validating last name
+		if (isset($_POST['l_name'])) 
+		{
+			$l_name = $_POST['l_name'];
+			$pattern = "/^[a-zA-Z- ]+$/";
+			if (strlen($l_name) > 0 && preg_match($pattern, $l_name))
+			{
+				//Adding last name to query
+				$selectquery .= "`lname`='$l_name' AND ";
+				$criteriacount++;
+			}
+		}
+
+		//Validating phone number
+		if (isset($_POST['phone'])) 
+		{
+			$phone = $_POST['phone'];
+			$phonepattern = "/^(\+?\(61\)|\(\+?61\)|\+?61|\(0[1-9]\)|0[1-9])?( ?-?[0-9]){7,9}$/";
+			if (strlen($phone) > 0 && preg_match($phonepattern, $phone))
+			{
+				//Validating 
+				$selectquery .= "`phone`='$phone' AND ";
+				$criteriacount++;
+			}
+		}
+
+		//Validating email
+		if (isset($_POST['email']))
+		{
+			$email = $_POST['email'];
+			$emailpattern = "/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/";
+			if (preg_match($emailpattern, $email))
+			{
+				//Adding email to query
+				$selectquery .= "`email`='$email' AND ";
+				$criteriacount++;
+			}
+		}
+
+		//Check if any search has been made.
+		if ($criteriacount > 0)
+		{
+			//Creating database connection
+			$conn = new mysqli($host, $user, $pswd, $db);
+
+			//Check if error with connection
+           	if ($conn->connect_errno)
+            {
+             	echo "<p>Failed to connect to database: " . $conn->connect_error . "</p>";
+                exit();
+            }
+
+           	//Query to create table if it does not exist
+           	$tablequery = "CREATE TABLE IF NOT EXISTS members (
+                'member_id' INT NOT NULL AUTO_INCREMENT,
+                'fname' varchar(20) NOT NULL,
+                'lname' varchar(20) NOT NULL,
+              	'email' varchar(255) NOT NULL,
+                'phone' varchar(255) NOT NULL,
+                PRIMARY KEY (member_id)
+            );";
+                    
+            //Execute table creation query
+            $conn->query($tablequery);
+
+			$selectquery = substr($selectquery, 0, -5);
+
+			$result = $conn->query($selectquery);
+
+			//Checking if result has any records/rows
+			if ($result->num_rows > 0)
+			{
+				while ($row = $result->fetch_assoc())
+				{
+					echo "<table>";
+					echo "<tr><td>Member ID: </td><td>" . $row['member_id'] . "</td></tr>";
+					echo "<tr><td>First Name: </td><td>" . $row['fname'] . "</td></tr>";
+					echo "<tr><td>Last Name: </td><td>" . $row['lname'] . "</td></tr>";
+					echo "<tr><td>Email: </td><td>" . $row['email'] . "</td></tr>";
+					echo "<tr><td>Phone: </td><td>" . $row['phone'] . "</td></tr>";
+					echo "</table>";
+				}
+			}
+			else
+			{
+				echo "<p>0 results:</p>";
+			}
+				
+			$conn->close();
+		}
+		else
+		{
+			echo "<p>No Search Terms Have Been Added</p>";
+		}
+	?>
+</body>
+</html>
---
+++ searchsalesprocess.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+    <meta name="Title" content="GotoGro Memeber Management Sysytem"/>
+   	<meta name="Authors" content="Sartaj Khan, Eddie Taing"/>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+	<?php
+		require_once("settings.php");
+
+		$selectquery = "SELECT * FROM `sales_records` WHERE ";
+		$criteriacount = 0;
+
+		//Checking if member id was set
+		if (isset($_POST['member_id']))
+		{
+			$member_id = $_POST['member_id'];
+			//Validating member id
+			if (is_numeric($member_id) && $member_id)
+			{
+				//Adding member id to query
+				$selectquery .= "`member_id`=$member_id AND ";
+				$criteriacount++;
+			}
+		}
+
+		//Checking if item name was set
+		if (isset($_POST['item_name']))
+		{
+			$item_name = $_POST['item_name'];
+			$pattern = "/^[a-zA-Z0-9- ]+$/";
+			//Validating item name
+			if (strlen($item_name) > 0 && preg_match($pattern, $item_name))
+			{
+				//Adding item name to query
+				$selectquery .= "`item_name`='$item_name' AND ";
+				$criteriacount++;
+			}
+		}
+
+		//Checking if item quantity was set
+		if (isset($_POST['item_quantity'])) 
+		{
+			$item_quantity = $_POST['item_quantity'];
+			//Validating item quantity
+			if (is_numeric($item_quantity))
+            {
+            	if ($item_quantity > 0)
+            	{	
+            		//Adding item quantity to query.
+            		$selectquery .= "`item_quantity` = $item_quantity AND ";
+            		$criteriacount++;
+            	}    
+            }
+		}
+
+		//Checking due date was set
+		if (isset($_POST['due_date'])) 
+		{
+			//Validating date
+            $due_date = $_POST['due_date'];
+            //https://stackoverflow.com/questions/30243775/get-date-from-input-form-within-php
+            if (!empty($due_date))
+            {
+            	$due_date = strtotime($due_date);
+                $due_date = date("Y-m-d", $due_date);
+
+                //Adding due date to query
+                $selectquery .= "`due_date`=$due_date AND ";
+                $criteriacount++;
+            }
+		}
+
+		//Checking if any search was added.
+		if ($criteriacount > 0)
+		{
+			//Creating database connection
+			$conn = new mysqli($host, $user, $pswd, $db);
+
+			//Check if error with connection
+           	if ($conn->connect_errno)
+            {
+             	echo "<p>Failed to connect to database: " . $conn->connect_error . "</p>";
+                exit();
+            }
+
+           	$tablequery = "CREATE TABLE IF NOT EXISTS sales_records (
+                sales_id INT NOT NULL AUTO_INCREMENT,
+                member_id INT NOT NULL,
+                item_name varchar(20) NOT NULL,
+                item_quantity INT NOT NULL,
+                due_date DATE NOT NULL,
+                PRIMARY KEY (sales_id),
+               	FOREIGN KEY (member_id) REFERENCES members(member_id)
+            );";
+                    
+            //Execute table creation query
+            $conn->query($tablequery);
+
+            //https://stackoverflow.com/questions/4915753/how-can-i-remove-three-characters-at-the-end-of-a-string-in-php
+			$selectquery = substr($selectquery, 0, -5);
+
+			$result = $conn->query($selectquery);
+
+			//Check if any rows in results
+			if ($result->num_rows > 0)
+			{
+				//Iterating through rows and adding to table.
+				while ($row = $result->fetch_assoc())
+				{
+					echo "<table>";
+					echo "<tr><td>Sales ID: </td><td>" . $row['sales_id'] . "</td></tr>";
+					echo "<tr><td>Member ID: </td><td>" . $row['member_id'] . "</td></tr>";
+					echo "<tr><td>Item Name: </td><td>" . $row['item_name'] . "</td></tr>";
+					echo "<tr><td>Item Quantity: </td><td>" . $row['item_quantity'] . "</td></tr>";
+					echo "<tr><td>Due Date: </td><td>" . $row['due_date'] . "</td></tr>";
+					echo "</table>";
+				}
+			}
+			else
+			{
+				echo "<p>0 results: ". $conn->error."</p>";
+			}
+				
+			$conn->close();
+		}
+		else
+		{
+			echo "<p>No Search Terms Have Been Added</p>";
+		}
+	?>
+</body>
+</html>
