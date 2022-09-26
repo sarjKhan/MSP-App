d4ad314d15e59c30091e952135b9d9a72670d1a4
sarjKhan <102861209+sarjKhan@users.noreply.github.com>
2022-09-26 22:02:03 +1000

    Changes v0.1

--- add_sales.php
+++ add_sales.php
@@ -11,30 +11,36 @@
 <body>
 	<h1>Add sales</h1>
 	<form action="addsalesprocess.php" method="POST">
-		<label>Member ID:</label>
-		<?php
-			if (isset($_GET['member_id'])) 
-			{
-				$member_id = $_GET['member_id'];
-				echo "<input type='text' name='member_id' id='member_id' value='$member_id'/>";
-			}
-			else
-			{;
-				echo "<input type='text' name='member_id' id='member_id'/>";
-			}
-		?>
-		
-		<br/>
-		<label>Item Name:</label>
-		<input type="text" name="item_name" id="item_name"/>
-		<br/>
-		<label>Item Quantity:</label>
-		<input type="text" name="item_quantity" id="item_quantity"/>
-		<br/>
-		<label>Due Date:</label>
-		<input type="date" name="due_date" id="due_date"/>
-		<br/>
-		<input type="submit" name="Submit"/>
+		<fieldset class="add_form">
+			<legend>Add Sales: </legend>
+			<label>Member ID:</label><br>
+			<?php
+				if (isset($_GET['member_id'])) 
+				{
+					$member_id = $_GET['member_id'];
+					echo "<input type='text' name='member_id' id='member_id' value='$member_id'/>";
+				}
+				else
+				{;
+					echo "<input type='text' name='member_id' id='member_id' placeholder = 'XXXXX'/>";
+				}
+			?>
+			
+			<br/>
+			<div class="input">
+				<label for="product">Product Name:&nbsp;</label><br>
+				<input type="text" name="product" placeholder="Name of product">
+			</div>
+			<div class="input">
+				<label for="qnt">Quantity:&nbsp;</label><br>
+				<input type="number" name="qnt" min=0 placeholder="Enter Quantity">
+			</div>
+			<div class="input">
+				<label for="due_date">Due Date:&nbsp;</label><br>
+				<input type="date" name="due_date" placeholder="xx/xx/xxxx">
+			</div>
+			<button type="submit" name="Submit" class="button">Add</button>
+		</fieldset>
 	</form>
 </body>
 </html>
